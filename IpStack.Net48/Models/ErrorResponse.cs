5697ccab742f01e6befade77b7d5b19b3bf9fb55
GohelGaurav <42742425+GohelGaurav@users.noreply.github.com>
2020-04-21 17:22:08 +0530

    Added .Net 4.5 And .Net 4.8 Support

---
+++ IpStack.Net48/Models/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IpStack.Net48.Models
+{
+    public class ErrorResponse
+    {
+        public bool Success { get; set; } = true;
+
+        public Error Error { get; set; }
+    }
+}
